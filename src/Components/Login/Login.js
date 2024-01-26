aa5546ba59f0737f432af5d921ddf69f81e7baf1
Riddhi Rustagi <riddhi147btcse22@igdtuw.ac.in>
2024-01-26 20:46:33 +0530

    Committing changes to Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../Header/header';
 import './Login.css';
+import Footer from '../Footer/footer';
 export const Login = () => {
     return (
         <>
@@ -17,6 +18,7 @@ export const Login = () => {
                 <p class="small-text">Don't have an account? <a href="signup.html" target="_blank">Sign Up</a></p>      
         </div>
         </div>
+        <Footer/>
     </>
     );
 };
