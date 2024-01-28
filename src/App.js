6b2ff0fbe5651c8cb7eb2a84b3c78f833e7d8e24
Riddhi Rustagi <riddhi147btcse22@igdtuw.ac.in>
2024-01-28 12:35:25 +0530

    insights page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { Register } from './Components/Register/Register';
 import { Login } from './Components/Login/Login';
 import { Home } from './Components/Home/Home';
 import Calculator from './Components/Calculator/Calculator';
+import Insight from './Components/Insights/Insights';
+import Header from './Components/Header/header';
 
 function App() {
   return (
     <div>
-      <Home/>
+      
+      <Insight/>
+      
     </div>
   )
 }
---
+++ src/Components/Insights/Insights.js
@@ -0,0 +1,94 @@
+import React from "react";
+import "./Insights.css";
+import Header from "../Header/header";
+import Footer from "../Footer/footer";
+
+function Insights() {
+  return (
+    <>
+      <Header />
+      <div className="insight-page">
+        <div className="video-background">
+          <video autoPlay loop muted>
+            <source src="pexels_videos_2169880 (2160p).mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="insight-content">
+          <div className="left-content">
+            <h2>Carbon Footprint</h2>
+            <p className="para Right">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className="right-content">
+            <p className="para left">
+              Paragraph Left 1 Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+              occaecat cupidatat non proident, sunt in culpa qui officia
+              deserunt mollit anim id est laborum.
+            </p>
+            <h2>Sustainability</h2>
+          </div>
+        </div>
+
+        {/* Card Container and Cards */}
+        <div className="card-container">
+          <h2>Steps towards Sustainability</h2>
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="reuse.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="energyC.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="Ptrasnsport.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="water.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Insights;
